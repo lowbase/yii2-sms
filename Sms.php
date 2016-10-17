e28476822852a52d7a59c293c330ef78136841ad
lowBase <lowbase@yandex.ru>
2016-10-18 00:44:30 +0300

    Update Sms.php

--- Sms.php
+++ Sms.php
@@ -94,7 +94,7 @@ class Sms extends Object
 		if (!isset(\Yii::$app->i18n->translations['sms']) && !isset(\Yii::$app->i18n->translations['sms/*'])) {
 			\Yii::$app->i18n->translations['sms'] = [
 				'class' => 'yii\i18n\PhpMessageSource',
-				'basePath' => '/lowbase/components/sms/messages',
+				'basePath' => '@vendor/lowbase/yii2-sms/messages',
 				'forceTranslation' => true,
 				'fileMap' => [
 					'sms' => 'sms.php'
